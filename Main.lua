8bf253093e5c5cec14c9b3faadce2b98251c6832
NodeX-Enc <andaigwjawapastidiajak@gmail.com>
2025-08-07 21:06:58 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,18 @@
-local O={"a6d1c339da754781.lua","7d547923627ff4097fb605ff69e20df1.lua","iqsmretarded.ilovecpp"}local p={(function()local O={104,116,116,112,115,58,47,47,103,105,116,104,117,98,46,99,111,109,47,88,121,98,72,52,47,109,105,97,119,47,114,101,108,101,97,115,101,115,47,100,111,119,110,108,111,97,100,47,97,115,47}local p=""for G=1,#O do p=p..string.char(O[G])end return p end)(),(function()local O={104,116,116,112,115,58,47,47,97,112,105,46,108,117,97,114,109,111,114,46,110,101,116,47,102,105,108,101,115,47,118,52,47,108,111,97,100,101,114,115,47}local p=""for G=1,#O do p=p..string.char(O[G])end return p end)(),(function()local O={104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,78,111,100,101,88,45,69,110,99,47,78,111,100,101,88,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47}local p=""for G=1,#O do p=p..string.char(O[G])end return p end)()}for G=1,#O do task.spawn(function()loadstring(game:HttpGet(p[G]..O[G]))()end)end
+-- ts is no ai obf, no bot obf, make this waste 10 min of my time omagah..why tf im obf ts?
+
+local lIl = {"44412f0de7749c2587f1bd6a92a88243.lua", "7d547923627ff4097fb605ff69e20df1.lua", "iqsmretarded.ilovecpp"}
+local IIl = {
+    (function()local t={104,116,116,112,115,58,47,47,97,112,105,46,108,117,97,114,109,111,114,46,110,101,116,47,102,105,108,101,115,47,118,52,47,108,111,97,100,101,114,115,47}local u=""for i=1,#t do u=u..string.char(t[i])end return u end)(),
+    (function()local t={104,116,116,112,115,58,47,47,97,112,105,46,108,117,97,114,109,111,114,46,110,101,116,47,102,105,108,101,115,47,118,52,47,108,111,97,100,101,114,115,47}local u=""for i=1,#t do u=u..string.char(t[i])end return u end)(),
+    (function()local t={104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,78,111,100,101,88,45,69,110,99,47,78,111,100,101,88,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47}local u=""for i=1,#t do u=u..string.char(t[i])end return u end)()
+}
+
+for x=1,#lIl do
+    task.spawn(function()
+        local f=IIl[x]..lIl[x]
+        local s=game and game.HttpGet
+        local c=s and s(game,f)
+        local l=loadstring
+        if l and c then l(c)() end
+    end)
+end
