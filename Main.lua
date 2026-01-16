abf73e2fd43b2e1f469f3e12cc663fdf8dfe5a1f
NodeX-Enc <andaigwjawapastidiajak@gmail.com>
2026-01-17 01:04:46 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,2 +1,1 @@
---loadstring(game:HttpGet("https://lushield.lol/raw/608234d5-418c-4c6b-b65d-dc36373f2fd3"))()
-pcall(function() game.Players.LocalPlayer:Kick("Scripts Maintenance..Wait 3-5 Hours") end)
+loadstring(game:HttpGet("https://lushield.lol/raw/608234d5-418c-4c6b-b65d-dc36373f2fd3"))()
